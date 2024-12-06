3fb00bd71b92da7e9ccfc7ec77984a7445cae224
gwenstew <gwenstew29@oulook.com>
2024-12-06 15:10:06 -0500

    implementing magiccard parent function

---
+++ src/components/MagicCard.tsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+import { Magic } from "../magic";
+//import MagicCardChild from "MagicCardChild";
+
+export default function MagicCard() {
+    const [cards, setCards] = useState<Magic[]>([]);
+
+    useEffect(() => {
+        async function fetchData(): Promise<void> {
+            try{
+                const rawData = await fetch("https://api.magicthegathering.io/v1/cards");
+                const data  = await rawData.json();
+                setCards(data.cards);
+            } catch (error) {
+                console.log("Error fetching data" + error);
+            }
+        }
+        fetchData();
+    });
+
+    return(
+        <MagicCard cards={cards}/>
+    )
+}
