ff7b9d55baca07230770448591ed9a83de975d1d
Madeline Wong <mwong20@bu.edu>
2024-12-06 15:03:48 -0500

    Changes to App.tsx to include MagicCard component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
+import MagicCard from "./components/MagicCard.tsx";
+import styled from "styled-components";
+
+//parent div for the app as a whole, containing all the data displayed from the API 
+const ParentDiv=styled.div`
+    width: 90vw;
+    margin: auto;
+    border: 5px black solid;
+`;
+
 export default function App(){
-  return(
-    <>
-    </>
-  )
+    return(
+        <ParentDiv>
+            <MagicCard/>
+        </ParentDiv>
+    )
 }
