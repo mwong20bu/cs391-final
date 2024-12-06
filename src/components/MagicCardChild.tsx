ca74451e5d2148cc6308e8f8d5744ca662ea49e8
Madeline Wong <mwong20@bu.edu>
2024-12-06 14:55:26 -0500

    Updated to MagicCardChild.tsx

---
+++ src/components/MagicCardChild.tsx
@@ -0,0 +1,47 @@
+import styled from "styled-components";
+import {Magic} from "../magic";
+
+//child component used for displaying info / UI
+
+//container for the character displays
+const AllCharsDiv=styled.div`
+    display: flex;
+    flex-flow: row wrap;    
+    justify-content: space-evenly;
+    background-color: teal;
+`;
+
+//styling div for each individual character
+const SingleCharDiv=styled.div`
+    display: flex;
+    flex-direction: column;   
+    justify-content: center;
+    width: 25%;
+    padding: 1%;
+    margin: 1%;
+    background-color: black;
+    color: white;
+    border: 'lightgreen 3px solid')};
+    font: small-caps bold calc(2px + 1vw) Arial, fantasy;
+    text-align: center;
+`;
+
+export default function MagicCardChild(props : { data:Magic[] } ){
+    return (
+        <AllCharsDiv >
+            {
+                props.data.map((char: Magic) =>
+                    <SingleCharDiv key={char.id}>
+                        <h1>{char.name}</h1>
+                        <img src={char.image} alt={`image of ${char.name}`} />
+                        <p>Rarity: {char.rarity}</p>
+                        <p>Converted mana cost: {char.cmc}</p>
+                        <p>Power: {char.power}</p>
+                        <p>Toughness: {char.toughness}</p>
+                        <p>Card Number: {char.cardnumber}</p>
+                    </SingleCharDiv>
+                )
+            }
+        </AllCharsDiv>
+    );
+}
