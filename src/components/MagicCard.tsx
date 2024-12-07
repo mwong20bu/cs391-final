3858d927632f8c6a59226d2f17982c8e2e84e8c9
gwenstew <gwenstew29@oulook.com>
2024-12-07 14:07:25 -0500

    adding comments

--- src/components/MagicCard.tsx
+++ src/components/MagicCard.tsx
@@ -1,8 +1,12 @@
+//implemented by Gwen Stewart
+
 import { useEffect, useState } from "react";
 import { Magic } from "../magic";
 import MagicCardChild from "./MagicCardChild";
 
+//function to fetch all magic card data from magic:the gathering API
 export default function MagicCard() {
+    //useState hook to store the magic card data
     const [cards, setCards] = useState<Magic[]>([]);
 
     useEffect(() => {
@@ -12,6 +16,7 @@ export default function MagicCard() {
                 const data  = await rawData.json();
                 setCards(data.cards);
             } catch (error) {
+                //an error occurred while trying to fetch data
                 console.log("Error fetching data" + error);
             }
         }
